d30fa145016ccf923865e7d3d644f3cb157e9bdb
Marmatee <Marmatee@Marcellos-MacBook-Pro.local>
2016-12-28 22:33:59 +0000

    Futile attempt comes to an end
    After spending the best part of a day troubleshooting why the image
    menu would open up in dev but not on the iCloud web page, I have
    discovered a conflict between dropit.js and iCloud dependency
    javascript-packed.js.
    
    Going to start the search for a new image menu tomorrow

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,16 +1,15 @@
+$('head').append("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>");
+
 // Put any pic you want in here
 var bg = "http://i.imgur.com/Ve1wAfQ.jpg";
 
 $('body').css('background','url('+bg+')');
 
 $('body').append('<ul class="menu"><li><a href="#"><i class="fa fa-picture-o big"></i></a><ul>');
-$('body').append('<li><a href="#"><img src="http://i.imgur.com/DYm1aqos.jpg"></a></li>');
-$('body').append('<li><a href="#"><img src="http://i.imgur.com/pWNgnmOs.jpg"></a></li>');
-$('body').append('<li><a href="#"><img src="http://i.imgur.com/09iertas.jpg"></a></li>');
-$('body').append('<li><a href="#"><img src="http://i.imgur.com/ipqicfms.jpg"></a></li>');
+$('body').append('<li class="daylist"><a href="#"><img src="http://i.imgur.com/DYm1aqos.jpg"></a></li>');
+$('body').append('<li class="daylist"><a href="#"><img src="http://i.imgur.com/pWNgnmOs.jpg"></a></li>');
+$('body').append('<li class="daylist"><a href="#"><img src="http://i.imgur.com/09iertas.jpg"></a></li>');
+$('body').append('<li class="daylist"><a href="#"><img src="http://i.imgur.com/ipqicfms.jpg"></a></li>');
 $('body').append('</ul></li></ul>');
 
 $('body').append("<script>$(document).ready(function(){$('.menu').dropit()});</script>");
-
-$('head').append("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>");
-$('head').append("<link rel='stylesheet' href=''>");
