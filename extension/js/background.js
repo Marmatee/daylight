0911a0f514114dfe0458236a98dc7d651d4f11de
Marmatee <Marmatee@Marcellos-MacBook-Pro.local>
2016-12-28 17:28:55 +0000

    hideous mess but almost at a working frontend

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,38 +1,16 @@
 // Put any pic you want in here
 var bg = "http://i.imgur.com/Ve1wAfQ.jpg";
 
-// var image_array = new Array();
-    // image_array[0] = "http://i.imgur.com/fLVXu6r.png";
-    // image_array[1] = "http://i.imgur.com/ipqicfm.jpg";
-    // image_array[2] = "http://i.imgur.com/09ierta.jpg";
-    // image_array[0] = "http://i.imgur.com/pWNgnmO.jpg";
-    // image_array[4] = "http://i.imgur.com/DYm1aqo.jpg";
-    // image_array[5] = "http://i.imgur.com/2Doe6Uy.png";
-    // image_array[6] = "http://i.imgur.com/xzCFJr7.jpg";
-    // image_array[7] = "http://i.imgur.com/V1YXlor.png";
-    // image_array[8] = "http://i.imgur.com/QVeeFFZ.png";
-    // image_array[9] = "http://i.imgur.com/V4ZMkGf.jpg";
-    // image_array[10] = "http://i.imgur.com/Oooi3g3.jpg";
-    // image_array[11] = "http://i.imgur.com/PkZqm4k.jpg";
-    // image_array[12] = "http://i.imgur.com/t6IWqaX.jpg";
-    // image_array[13] = "http://i.imgur.com/blmHPfo.jpg";
-    // image_array[14] = "http://i.imgur.com/YyYEXAl.jpg";
-    // image_array[15] = "http://i.imgur.com/Ve1wAfQ.jpg";
+$('body').css('background','url('+bg+')');
 
-    // var rand_path = image_array[Math.floor(Math.random() * image_array.length)];
+$('body').append('<ul class="menu"><li><a href="#"><i class="fa fa-picture-o big"></i></a><ul>');
+$('body').append('<li><a href="#"><img src="http://i.imgur.com/DYm1aqos.jpg"></a></li>');
+$('body').append('<li><a href="#"><img src="http://i.imgur.com/pWNgnmOs.jpg"></a></li>');
+$('body').append('<li><a href="#"><img src="http://i.imgur.com/09iertas.jpg"></a></li>');
+$('body').append('<li><a href="#"><img src="http://i.imgur.com/ipqicfms.jpg"></a></li>');
+$('body').append('</ul></li></ul>');
 
-    // $(document).ready(function() {
-    //    $('body').css('background-image', 'url('+rand_path+')');
-    // });
+$('body').append("<script>$(document).ready(function(){$('.menu').dropit()});</script>");
 
-// Leaving randomiser out for a bit. Going to concentrate on manually selecting a picture for now.
-
-    $('body').css('background','url('+bg+')');
-
-var fa = document.createElement('fa')
-    fa.innerHTML = "<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>";
-    document.head.appendChild(fa);
-
-var uploader = document.createElement('uploader')
-    uploader.innerHTML = "<i class='fa fa-picture-o'></i>";
-    document.body.appendChild(uploader);
+$('head').append("<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>");
+$('head').append("<link rel='stylesheet' href=''>");
