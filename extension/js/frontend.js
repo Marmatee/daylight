267a52a4910a7329c831383cd3f19dfd5e2231ba
Marmatee <marcello.ciciriello@me.com>
2017-01-01 00:49:39 +0000

    learning from mistakes

--- extension/js/frontend.js
+++ extension/js/frontend.js
@@ -8,6 +8,7 @@ $('body').css('background','url('+bg+')');
 $('body').append('<div class="dropdown"> <input type="checkbox" id="checkbox-toggle"> <label for="checkbox-toggle" class="dropper"><i class="fa fa-picture-o" aria-hidden="true"></i></label> <ul class="switcher"> <li><a href="javascript:void(0)"><img src="http://i.imgur.com/DYm1aqos.jpg"></a></li><li><a href="javascript:void(0)"><img src="http://i.imgur.com/pWNgnmOs.jpg"></a></li><li><a href="javascript:void(0)"><img src="http://i.imgur.com/09iertas.jpg"></a></li><li><a href="javascript:void(0)"><img src="http://i.imgur.com/Ve1wAfQs.jpg"></a></li></ul></div>');
 
 
+
 // I know that's HTML - but believe me it's easier to uncomment that stuff, then
 // make any modifications I want to, then run it through http://www.willpeavy.com/minifier/
 // and paste it in the append shown above.
